a273ac25a9acf26b0956e181e25a0c434e973df3
Justin Dong <jdong1@sas.upenn.edu>
2021-10-31 15:43:30 -0400

    added test

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import store from './features/common/store';
+import { render, screen } from '@testing-library/react';
+// import { Provider } from 'react-redux';
+// import store from './features/common/store';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-  );
-
-  expect(getByText(/learn/i)).toBeInTheDocument();
+test('renders create group button', () => {
+  render(<App />);
+  const button = screen.getByText('Create Group');
+  expect(button).toBeInTheDocument();
 });
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(
+//     <Provider store={store}>
+//       <App />
+//     </Provider>,
+//   );
+
+//   expect(getByText(/learn/i)).toBeInTheDocument();
+// });
