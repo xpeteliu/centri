a68222cf11305c95c8a73e0b685f51c4311bd33b
Xiran Xu <xiranxu@seas.upenn.edu>
2021-11-07 14:45:30 -0500

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,31 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './App.css';
-// import LoginComponent from './features/Login/Login';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import HomePage from './components/HomePage/HomePage';
+import LoginPage from './components/LoginPage/LoginPage';
+import './App.css';
 
-// function App() {
-//   return (
-//     { LoginComponent }
-//   );
-// }
+const Home = () => (
+  <HomePage />
+);
 
-// export default App;
+const Login = () => (
+  <LoginPage />
+);
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
