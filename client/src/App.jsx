73b5fafaa5c6f1cfe9bcc62945fbc7b931742fe4
Xiran Xu <xiranxu@seas.upenn.edu>
2021-11-07 16:16:16 -0500

    COMMIT

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,19 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import LoginPage from './features/Login/Login'
-import RegistrationPage from './features/Registration/Registration'
-import HomePage from './features/HomePage/HomePage'
-
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LoginPage from './features/Login/Login';
+import RegistrationPage from './features/Registration/Registration';
+import HomePage from './features/HomePage/HomePage';
 
 export default function App() {
-    return (
-        <Router>
-            <div>
-                <Switch>
-                    <Route path="/login" component={ LoginPage } />
-                    <Route path="/register" component={ RegistrationPage } />
-                    <Route path="/home" component={ HomePage } />
-                </Switch>
-                <Footer />
-            </div>
-        </Router>
-    )
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/register" component={RegistrationPage} />
+          <Route path="/home" component={HomePage} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
-
