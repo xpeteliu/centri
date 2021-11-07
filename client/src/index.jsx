18d02feff3d976aa830c2b30885644131dd0367c
Xiran Xu <xiranxu@seas.upenn.edu>
2021-11-07 18:20:15 -0500

    router in js

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import store from './features/common/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
