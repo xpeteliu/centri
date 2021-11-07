e2db51c3721c84f19b83b3ff6c4321ee5980a2a6
Xiran Xu <xiranxu@seas.upenn.edu>
2021-11-07 14:45:30 -0500

    commit

--- client/src/features/Login/Login.js
+++ client/src/features/Login/Login.js
@@ -1,20 +1,16 @@
-function LoginComponent() {
-  const loginForm = document.getElementById('login-form');
-  const loginButton = document.getElementById('login-form-submit');
-  const loginErrorMsg = document.getElementById('login-error-msg');
-  return (
-    loginButton.addEventListener('click', (e) => {
-      e.preventDefault();
-      const username = loginForm.username.value;
-      const password = loginForm.password.value;
-      if (username === 'user' && password === 'web_dev') {
-        alert('You have successfully logged in.');
-        // location.reload();
-      } else {
-        loginErrorMsg.style.opacity = 1;
-      }
-    })
-  );
+import React, { Component } from 'react';
+import LoginForm from './LoginForm';
+
+import './LoginPage.css';
+
+class LoginPage extends Component {
+  render() {
+    return (
+      <div className="LoginPage">
+        <LoginForm />
+      </div>
+    );
+  }
 }
 
-export default LoginComponent;
+export default LoginPage;
