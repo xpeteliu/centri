1743cfb96f6e70803cbb0c7096a1d7353fd71275
Xiran Xu <xiranxu@seas.upenn.edu>
2021-11-07 15:55:20 -0500

    login-resgistration

--- client/src/features/HomePage/HomePage.js
+++ client/src/features/HomePage/HomePage.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
-import NavBar from '../NavBar/NavBar';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-class HomePage extends Component {
-  render() {
+export default function HomePage() {
     return (
-      <div className="HomePage">
-        <NavBar />
-        <h2>Home Page</h2>
-      </div>
-    );
-  }
+        <div className="text-center">
+            <h1 className="main-title home-page-title">Welcome to Our App</h1>
+            <Link to="/">
+                <button className="primary-button">Log out</button>
+            </Link>
+        </div>
+    )
 }
-
-export default HomePage;
--- client/src/features/Login/Login.js
+++ client/src/features/Login/Login.js
@@ -1,16 +1,35 @@
-import React, { Component } from 'react';
-import LoginForm from './LoginForm';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../App.css';
 
-import './LoginPage.css';
-
-class LoginPage extends Component {
-  render() {
-    return (
-      <div className="LoginPage">
-        <LoginForm />
-      </div>
-    );
-  }
+export default function SignInPage() {
+  return (
+    <div className="text-center m-5-auto">
+      <h2>Sign in to us</h2>
+      <form action="/home">
+        <p>
+          <>{ /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }</>
+          <label>Username or email address</label>
+          {/* <br/> */}
+          <input type="text" name="first_name" required />
+        </p>
+        <p>
+          <>{ /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }</>
+          <label>Password</label>
+          <>{ /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }</>
+          <Link to="/forget-password"><label className="right-label">Forget password?</label></Link>
+          {/* <br/> */}
+          <input type="password" name="password" required />
+        </p>
+        <p>
+          <button id="sub_btn" type="submit">Login</button>
+        </p>
+      </form>
+      <footer>
+        <p>First time?</p>
+        <p><Link to="/register">Create an account</Link></p>
+        <p><Link to="/">Back to Homepage</Link></p>
+      </footer>
+    </div>
+  );
 }
-
-export default LoginPage;
