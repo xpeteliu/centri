98ce1f6c009acffd314bca34aee1430a61a8d985
Xiran Xu <xiranxu@seas.upenn.edu>
2021-11-07 16:16:16 -0500

    COMMIT

--- client/src/features/Registration/Registration.js
+++ client/src/features/Registration/Registration.js
@@ -1,32 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function SignUpPage() {
-
-    return (
-        <div className="text-center m-5-auto">
-            <h5>User Registration</h5>
-            <form action="/home">
-                <p>
-                    <label>Username</label><br/>
-                    <input type="text" name="first_name" required />
-                </p>
-                <p>
-                    <label>Email address</label><br/>
-                    <input type="email" name="email" required />
-                </p>
-                <p>
-                    <label>Password</label><br/>
-                    <input type="password" name="password" requiredc />
-                </p>
-                <p>
-                    <button id="sub_btn" type="submit">Register</button>
-                </p>
-            </form>
-            <footer>
-                <p><Link to="/">Back to Homepage</Link></p>
-            </footer>
-        </div>
-    )
-
+  return (
+    <div className="text-center m-5-auto">
+      <h2>Join us</h2>
+      <h5>Create your personal account</h5>
+      <form action="/home">
+        <p>
+          <>{ /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }</>
+          <label>Username</label>
+          <input type="text" name="first_name" required />
+        </p>
+        <p>
+          <>{ /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }</>
+          <label>Email address</label>
+          <input type="email" name="email" required />
+        </p>
+        <p>
+          <>{ /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }</>
+          <label>Password</label>
+          <input type="password" name="password" requiredc />
+        </p>
+        <p>
+          <button id="sub_btn" type="submit">Register</button>
+        </p>
+      </form>
+      <footer>
+        <p>
+          <Link to="/">Back to Homepage</Link>
+        </p>
+      </footer>
+    </div>
+  );
 }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,20 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { Provider } from 'react-redux';
-import Login from './features/Login/Login';
-import store from './features/common/store';
+import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Login />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'));
