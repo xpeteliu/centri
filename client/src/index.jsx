0a69eab4a6c8cbb2679694b1c0f43193275d7dd5
Xiran Xu <xiranxu@seas.upenn.edu>
2021-11-07 14:39:35 -0500

    env

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import App from './App';
+import Login from './features/Login/Login';
 import store from './features/common/store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Login />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
