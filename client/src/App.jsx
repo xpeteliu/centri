ca703c6ceca93a3eaff56589702c16fd7706601d
Xiran Xu <xiranxu@seas.upenn.edu>
2021-11-08 17:59:39 -0500

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import LoginPage from './features/Login/Login';
 import RegistrationPage from './features/Registration/Registration';
 import HomePage from './features/HomePage/HomePage';
@@ -7,11 +7,12 @@ import HomePage from './features/HomePage/HomePage';
 export default function App() {
   return (
     <div>
-      <Switch>
-        <Route path="/login" component={LoginPage} />
-        <Route path="/register" component={RegistrationPage} />
-        <Route path="/" component={HomePage} />
-      </Switch>
+      <Route exact path="/login" />
+      <LoginPage />
+      <Route exact path="/register" />
+      <RegistrationPage />
+      <Route exact path="/" />
+      <HomePage />
     </div>
   );
 }
