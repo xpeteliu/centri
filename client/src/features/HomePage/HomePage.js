acf6a8931441fed7947606f37cdac17162647ec2
Xiran Xu <xiranxu@seas.upenn.edu>
2021-11-12 15:25:42 -0500

    css

--- client/src/features/HomePage/HomePage.js
+++ client/src/features/HomePage/HomePage.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 
 export default function HomePage() {
   return (
     <div className="text-center">
-      <h1 className="main-title home-page-title">Welcome to Our App</h1>
+      <h1 className="home-page-title">Welcome to Our App</h1>
+      <Link to="/Login">
+        <Button variant="primary">Sign In</Button>
+      </Link>
       <Link to="/register">
-        <button type="button" className="primary-button">Sign In</button>
+        <Button variant="secondary">Sign Up</Button>
       </Link>
     </div>
   );
--- client/src/features/Login/Login.js
+++ client/src/features/Login/Login.js
@@ -7,13 +7,13 @@ export default function SignInPage() {
     <div className="text-center m-5-auto">
       <h2>Sign In</h2>
       <form action="/home">
-        <p>
+        <p id="userinfo">
           <label htmlFor="first_name">
             Username or email address
             <input id="first_name" type="text" name="first_name" required />
           </label>
         </p>
-        <p>
+        <p id="userinfo">
           <label htmlFor="password">
             Password
             <input id="password" type="password" name="password" required />
