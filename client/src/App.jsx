af60db68513ee8ca2de3df7b4b7855e165b693bd
Xiran Xu <xiranxu@seas.upenn.edu>
2021-11-12 13:49:41 -0500

    css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import SignInPage from './features/Login/Login';
 import SignUpPage from './features/Registration/Registration';
 import HomePage from './features/HomePage/HomePage';
@@ -7,9 +7,11 @@ import HomePage from './features/HomePage/HomePage';
 export default function App() {
   return (
     <div>
-      <Route exact path="/login"><SignInPage></SignInPage></Route>
-      <Route exact path="/register"><SignUpPage></SignUpPage></Route>
-      <Route exact path="/"><HomePage></HomePage></Route>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/login" component={SignInPage} />
+        <Route path="/register" component={SignUpPage} />
+      </Switch>
     </div>
   );
 }
--- client/src/features/HomePage/HomePage.js
+++ client/src/features/HomePage/HomePage.js
@@ -5,8 +5,8 @@ export default function HomePage() {
   return (
     <div className="text-center">
       <h1 className="main-title home-page-title">Welcome to Our App</h1>
-      <Link to="/">
-        <button type="button" className="primary-button">Log out</button>
+      <Link to="/register">
+        <button type="button" className="primary-button">Sign In</button>
       </Link>
     </div>
   );
