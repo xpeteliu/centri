34e31d761d0fb8b6fe695542d7d06a701d2c3060
Xiran Xu <xiranxu@seas.upenn.edu>
2021-11-14 15:06:47 -0500

    change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import { Route, Switch } from 'react-router-dom';
 import SignInPage from './features/Login/Login';
 import SignUpPage from './features/Registration/Registration';
 import HomePage from './features/HomePage/HomePage';
-import AddPost from './features/Post/AddPost';
 
 export default function App() {
   return (
@@ -12,7 +11,6 @@ export default function App() {
         <Route path="/" component={HomePage} />
         <Route path="/login" component={SignInPage} />
         <Route path="/register" component={SignUpPage} />
-        <Route path="/addPost" component={AddPost}/>
       </Switch>
     </div>
   );
