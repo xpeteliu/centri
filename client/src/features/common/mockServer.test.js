97c80bbda2befd542def8df037122135373ec530
xpeteliu <xpeteliu@outlook.com>
2021-11-14 19:29:58 -0500

    Change login success status code to 204

--- client/src/features/common/mockServer.test.js
+++ client/src/features/common/mockServer.test.js
@@ -6,11 +6,11 @@ import { rest } from 'msw';
 test.skip('Suppress Jest warning for empty test file', () => 1);
 
 const mockServer = setupServer(
-  rest.get('http://localhost:8000/user/login', (req, res, ctx) => res(
-    // Username: testName; Password: testPwd; mock SID: testSid
-    req.headers.get('Authorization') === 'Basic dGVzdE5hbWU6dGVzdFB3ZA=='
-      ? ctx.cookie('connect.sid', 'testSid') : ctx.status(401),
-  )),
+  // Username: testName; Password: testPwd; mock SID: testSid
+  rest.get('http://localhost:8000/user/login',
+    (req, res, ctx) => (req.headers.get('Authorization') === 'Basic dGVzdE5hbWU6dGVzdFB3ZA=='
+      ? res(ctx.status(204), ctx.cookie('connect.sid', 'testSid')) : res(ctx.status(401))
+    )),
 );
 
 export default mockServer;
