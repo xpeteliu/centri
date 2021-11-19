a39f6706f18afc00020e3b90a6f6b1e41eabaada
unknown <ddwang@seas.upenn.edu>
2021-11-19 15:15:10 -0500

    user list

--- client/src/features/Messages/Messages.js
+++ client/src/features/Messages/Messages.js
@@ -1,26 +1,36 @@
 /* eslint react/prop-types: 0 */
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Container, Col, Stack, Card,
 } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Messages() {
-  const [sender, setSender] = useState('');
+  // const tempUser = { id: 1, name: 'user1' };
+  const tempUsers = [
+    { id: 1, name: 'user1' },
+    { id: 2, name: 'user2' },
+    { id: 3, name: 'user3' },
+  ];
+  const [sender, setSender] = useState(0);
 
   const handleSelectSender = async (senderId) => {
     // await fetch messages from/to sender
-    console.log(sender);
+    console.log('id', senderId);
     setSender(senderId);
   };
 
+  useEffect(() => {
+    console.log(sender);
+  }, [sender]);
+
   return (
     <Container className="App">
-      <Col xs={4}>
+      <Col xs={2}>
         <Stack direction="vertical" gap={5}>
-          <SenderTab
-            senderId={3}
+          <UserList
+            users={tempUsers}
             onSelect={handleSelectSender}
           />
         </Stack>
@@ -29,19 +39,35 @@ function Messages() {
   );
 }
 
-function SenderTab({ senderId, onSelect }) {
-  function handleClick(event) {
-    onSelect(event.target.name);
-  }
-  const buttonName = `btnSelectUser${senderId}`;
+function UserList({ users, onSelect }) {
+  const rows = [];
+  users.forEach((user) => {
+    rows.push(
+      <UserTab
+        userId={user.id}
+        userName={user.name}
+        onSelect={onSelect}
+      />,
+    );
+  });
+  return (
+    <Stack direction="vertical" gap={5}>
+      {rows}
+    </Stack>
+  );
+}
+
+function UserTab({ userId, userName, onSelect }) {
+  const handleClick = () => {
+    onSelect(userId);
+  };
   return (
     <Container className="App">
-      <Card>
+      <Card onClick={handleClick} style={{ cursor: 'pointer' }}>
         <Card.Body>
           <Card.Text>
-            Click me
+            {userName}
           </Card.Text>
-          <button name={buttonName} type="button" onClick={handleClick}>user</button>
         </Card.Body>
       </Card>
     </Container>
