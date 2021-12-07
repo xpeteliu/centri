3ce52816d85ac3f38ef0da7a36dba7c602e0085e
xpeteliu <xpeteliu@outlook.com>
2021-12-07 15:04:26 -0500

    Restructure backend code

--- api/src/router/userRouter.js
+++ api/src/router/userRouter.js
@@ -1,156 +1,26 @@
 import express from 'express';
-import passport from 'passport';
-import User from '../model/User';
-import Group from '../model/Group';
+import * as userController from '../controller/userController';
 
 const userRouter = express.Router();
 
-userRouter.post('/', async (req, res) => {
-  try {
-    const saved = await (new User(req.body)).save();
-    res.status(200)
-      .json(saved);
-  } catch (e) {
-    res.status(e.code === 11000 ? 409 : 400)
-      .json({ message: e.message });
-  }
-});
+userRouter.post('/', userController.addUser);
 
-userRouter.post(
-  '/login',
-  (req, res, next) => {
-    passport.authenticate('local', (err, user, info) => {
-      if (err) {
-        next(err);
-        return;
-      }
-      if (!user) {
-        res.status(401)
-          .json(info);
-        return;
-      }
-      req.login(user, (error) => {
-        if (error) {
-          next(error);
-          return;
-        }
-        res.json({
-          code: 0,
-          id: user.id,
-        });
-      });
-    })(req, res, next);
-  },
-);
+userRouter.post('/login', userController.loginUser);
 
-userRouter.post(
-  '/logout',
-  (req, res) => {
-    req.logout();
-    res.status(204)
-      .end();
-  },
-);
+userRouter.post('/logout', userController.logoutUser);
 
-userRouter.post('/filter/paginate', async (req, res) => {
-  try {
-    res.json(await User.find(req.body.filter)
-      .sort(req.body.sort)
-      .skip(req.body.skip)
-      .limit(req.body.limit)
-      .exec());
-  } catch (e) {
-    res.status(400)
-      .json({ message: e.message });
-  }
-});
+userRouter.post('/filter/paginate', userController.queryUsers);
 
-userRouter.post('/filter/count', async (req, res) => {
-  try {
-    res.json({
-      ...req.body,
-      count: await User.countDocuments(req.body.filter)
-        .exec(),
-    });
-  } catch (e) {
-    res.status(400)
-      .json({ message: e.message });
-  }
-});
+userRouter.post('/filter/count', userController.countUsers);
 
-userRouter.get('/:userId', async (req, res) => {
-  try {
-    const result = await User.findById(req.params.userId)
-      .exec();
-    if (result == null) {
-      res.status(404)
-        .json({ message: 'No record found' });
-      return;
-    }
-    res.json(result);
-  } catch (e) {
-    res.status(400)
-      .json({ message: e.message });
-  }
-});
+userRouter.get('/:userId', userController.findUser);
 
-userRouter.put('/:userId', async (req, res) => {
-  try {
-    if ((req.user == null || req.params.userId !== req.user.id)
-      && (!(Object.keys(req.body).length === 1 && req.body.password))) {
-      res.status(401)
-        .json({ message: 'Not operating as an authorized user' });
-      return;
-    }
-    res.json(await User.findByIdAndUpdate(req.params.userId, req.body)
-      .exec());
-  } catch (e) {
-    res.status(400)
-      .json({ message: e.message });
-  }
-});
+userRouter.put('/:userId', userController.updateUser);
 
-userRouter.delete('/:userId', async (req, res) => {
-  try {
-    if (req.user == null || req.params.userId !== req.user.id) {
-      res.status(401)
-        .json({ message: 'Not operating as an authorized user' });
-      return;
-    }
-    res.json(await User.findByIdAndDelete(req.params.userId)
-      .exec());
-  } catch (e) {
-    res.status(400)
-      .json({ message: e.message });
-  }
-});
+userRouter.delete('/:userId', userController.removeUser);
 
-userRouter.get('/:userId/joinedGroup', async (req, res) => {
-  try {
-    res.json(await Group.find({
-      $or: [
-        { creatorId: req.params.userId },
-        { adminIds: req.params.userId },
-        { memberIds: req.params.userId },
-      ],
-    })
-      .exec());
-  } catch (e) {
-    res.status(400)
-      .json({ message: e.message });
-  }
-});
+userRouter.get('/:userId/joinedGroup', userController.queryUserJoinedGroups);
 
-userRouter.get('/:userId/pendingGroup', async (req, res) => {
-  try {
-    res.json(await Group.find(
-      { pendingMemberIds: req.params.userId },
-    )
-      .exec());
-  } catch (e) {
-    res.status(400)
-      .json({ message: e.message });
-  }
-});
+userRouter.get('/:userId/pendingGroup', userController.queryUserPendingGroups);
 
 export default userRouter;
