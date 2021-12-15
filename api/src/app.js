2920a54397fe591de5d4d9bfa6fe4d0009157b85
xpeteliu <xpeteliu@outlook.com>
2021-12-14 21:04:17 -0500

    Add default secret value

--- api/src/app.js
+++ api/src/app.js
@@ -29,7 +29,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.resolve('./build')));
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.SESSION_SECRET || 'test',
   resave: false,
   saveUninitialized: false,
   cookie: {
